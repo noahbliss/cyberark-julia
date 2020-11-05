c794258294febbea91423090cfb5052b23e23898
Noah <noah@superuser.sh>
2020-11-05 14:53:40 -0500

    Reworked how we deal with incoming data.

--- client.jl
+++ client.jl
@@ -24,7 +24,7 @@ function login(pvwahost, pvwauri, causer, capass)
         else
                 capass = convert(Base.SecretBuffer, capass)
         end
-        url = "$pvwauri/api/auth/ldap/logon"
+        uri = "$pvwauri/api/auth/ldap/logon"
         headers = ["Content-Type" => "application/json"]
         payload = JSON.json(Dict(
                 "username" => causer,
@@ -34,7 +34,7 @@ function login(pvwahost, pvwauri, causer, capass)
                 "secureMode" => true,
                 "additionalInfo" => ""
         ))
-        response = HTTP.request("POST", url, headers, payload; require_ssl_verification = false, cookies = true, cookiejar = cookiejar)
+        response = HTTP.request("POST", uri, headers, payload; require_ssl_verification = false, cookies = true, cookiejar = cookiejar)
         Base.shred!(capass)
         if response.status == 200
                 for set in cookiejar[pvwahost]
@@ -49,9 +49,9 @@ function login(pvwahost, pvwauri, causer, capass)
 end
 
 function webreq(pvwauri, cookiejar, headerauth, query)
-        url = "$pvwauri/api/$query"
+        uri = "$pvwauri/api/$query"
         headers = ["Content-Type" => "application/json", "X-CA66666" => headerauth ]
-        response = HTTP.request("GET", url, headers; require_ssl_verification = false, cookies = true, cookiejar = cookiejar)
+        response = HTTP.request("GET", uri, headers; require_ssl_verification = false, cookies = true, cookiejar = cookiejar)
         #return String(response.body)
         if response.status == 200
                 return JSON.parse(String(response.body))
@@ -61,7 +61,7 @@ function webreq(pvwauri, cookiejar, headerauth, query)
         # return response.body
 end
 
-#Make webreq a little more friendly
+# Make webreq a little more friendly
 function request(req)
         try response = webreq(pvwauri, cookiejar, headerauth, req)
                 return response
@@ -75,8 +75,30 @@ function request(req)
         end
 end
 
-#Try a login initially.
+# Try a login initially.
 headerauth = login(pvwahost, pvwauri, causer, capass)
 
-#List accounts.
-action = request("ExtendedAccounts")
+# List accounts. This variable stores the original format from the CyberArk API.
+accounts = request("ExtendedAccounts")
+
+# Initialize our view.
+viewlist = Dict()
+
+for acc in accounts["Accounts"]
+        # Alias data for sanity's sake
+        safe = (try acc["Properties"]["Safe"] catch; nothing end)
+        addr = (try acc["Properties"]["Address"] catch; nothing end)
+        user = (try acc["Properties"]["UserName"] catch; nothing end)
+        name = (try acc["Properties"]["Name"] catch; nothing end)
+        id = (try acc["AccountID"] catch; nothing end)
+        # Add the root key for the safe if it doesn't exist yet.
+        try viewlist[safe] catch; viewlist[safe] = Dict() end
+        # Add our account
+        viewlist[safe][name] = Dict(
+                "Address" => addr,
+                "Username" => user,
+                "ID" => id
+        )
+end
+
+viewlist
