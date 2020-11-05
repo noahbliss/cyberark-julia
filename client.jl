fbf54eb81675e27c232fc5137df71cbfbb2018c7
Noah <noah@superuser.sh>
2020-11-05 16:45:15 -0500

    Successful RDP brokering without using a web browser!!

--- client.jl
+++ client.jl
@@ -16,6 +16,11 @@ pvwauri = a2var("pvwauri", importedvars)
 causer = a2var("causer", importedvars)
 capass = a2var("capass", importedvars) #We'll replace this with a SecretBuffer almost immediately.
 
+# Used in makedatastructures
+viewlist = Dict()
+accountlist = Dict()
+
+# Used in most API calls.
 cookiejar = Dict{String, Set{HTTP.Cookie}}()
 
 function login(pvwahost, pvwauri, causer, capass)
@@ -75,30 +80,78 @@ function request(req)
         end
 end
 
+function getpsm(pvwauri, cookiejar, headerauth, accountid, reason, target)
+        uri = "$pvwauri/api/Accounts/$accountid/PSMConnect"
+        headers = ["Content-Type" => "application/json", "X-CA66666" => headerauth ]
+        payload = JSON.json(Dict(
+                "reason" => reason,
+                "ConnectionComponent" => "PSM-RDP",
+                "ConnectionParams" => Dict(
+                        "AllowMappingLocalDrives" => Dict(
+                                "value" => "Yes",
+                                "ShouldSave" => false
+                        ),
+                        "PSMRemoteMachine" => Dict(
+                                "value" => target,
+                                "ShouldSave" => false
+                        )
+                )
+        ))
+        response = HTTP.request("POST", uri, headers, payload; require_ssl_verification = false, cookies = true, cookiejar = cookiejar)
+        if response.status == 200
+                #return JSON.parse(String(response.body))
+                return String(response.body)
+        else
+                error(response.status)
+        end
+end
+
+function makedatastructures(caaccounts)
+        for acc in caaccounts["Accounts"]
+                # Alias data for sanity's sake
+                safe = (try acc["Properties"]["Safe"] catch; nothing end)
+                addr = (try acc["Properties"]["Address"] catch; nothing end)
+                user = (try acc["Properties"]["UserName"] catch; nothing end)
+                name = (try acc["Properties"]["Name"] catch; nothing end)
+                id = (try acc["AccountID"] catch; nothing end)
+                # Add the root key for the safe if it doesn't exist yet.
+                try viewlist[safe] catch; global viewlist[safe] = Dict() end
+                # Add our account
+                global viewlist[safe][name] = Dict(
+                        "Address" => addr,
+                        "Username" => user,
+                        "AccountID" => id
+                )
+                global accountlist[name] = Dict(
+                        "Address" => addr,
+                        "Username" => user,
+                        "AccountID" => id,
+                        "Safe" => safe
+                )
+        end
+        return
+end
+
+
 # Try a login initially.
 headerauth = login(pvwahost, pvwauri, causer, capass)
 
 # List accounts. This variable stores the original format from the CyberArk API.
-accounts = request("ExtendedAccounts")
+caaccounts = request("ExtendedAccounts")
+# Make our format data structures.
+makedatastructures(caaccounts)
+#accountlist and viewlist
 
-# Initialize our view.
-viewlist = Dict()
 
-for acc in accounts["Accounts"]
-        # Alias data for sanity's sake
-        safe = (try acc["Properties"]["Safe"] catch; nothing end)
-        addr = (try acc["Properties"]["Address"] catch; nothing end)
-        user = (try acc["Properties"]["UserName"] catch; nothing end)
-        name = (try acc["Properties"]["Name"] catch; nothing end)
-        id = (try acc["AccountID"] catch; nothing end)
-        # Add the root key for the safe if it doesn't exist yet.
-        try viewlist[safe] catch; viewlist[safe] = Dict() end
-        # Add our account
-        viewlist[safe][name] = Dict(
-                "Address" => addr,
-                "Username" => user,
-                "ID" => id
-        )
-end
 
 viewlist
+accname = "LONGNAME_OF_ACCOUNT"
+target = "FQDN_OR_IP_OF_TARGET"
+
+accountid = accountlist[accname]["AccountID"]
+psmstr = getpsm(pvwauri, cookiejar, headerauth, accountid, reason, target)
+
+filename = "rdpswap.rdp"
+open(filename, "w")
+write(filename, psmstr)
+run(`ca-rdp $filename`)
