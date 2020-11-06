03af32d844661901a17790d8d874b8542fd23be7
Noah <noah@superuser.sh>
2020-11-06 18:02:24 -0500

    Updated to use the new package. Not yet tested.

--- client.jl
+++ client.jl
@@ -2,6 +2,7 @@
 import HTTP
 import JSON
 using DelimitedFiles
+using CyberArkPVWAClient
 
 #settingsfile = "settings.conf"
 backupsettingsfile = "$(homedir())/.config/cyberark-julia/settings.conf"
@@ -11,102 +12,15 @@ isfile(settingsfile) || println("Settings file is missing.") && exit(1)
 importedvars = readdlm(settingsfile, '=', String; skipblanks=true)
 a2var(key, a) = (c=1; for i in a[:, 1]; i == key && return a[c, 2]; c+=1; end || error("$key not found"))
 
-pvwahost = a2var("pvwahost", importedvars)
 pvwauri = a2var("pvwauri", importedvars)
+method = a2var("method", importedvars)
 causer = a2var("causer", importedvars)
-capass = a2var("capass", importedvars) #We'll replace this with a SecretBuffer almost immediately.
 
-# Used in makedatastructures
-viewlist = Dict()
-accountlist = Dict()
 
-# Used in most API calls.
-cookiejar = Dict{String, Set{HTTP.Cookie}}()
-
-function login(pvwahost, pvwauri, causer, capass)
-        if capass == ""
-                capass = Base.getpass("Please enter your CyberArk password")
-        else
-                capass = convert(Base.SecretBuffer, capass)
-        end
-        uri = "$pvwauri/api/auth/ldap/logon"
-        headers = ["Content-Type" => "application/json"]
-        payload = JSON.json(Dict(
-                "username" => causer,
-                "password" => read(capass, String),
-                "newPassword" => nothing,
-                "type" => "ldap",
-                "secureMode" => true,
-                "additionalInfo" => ""
-        ))
-        response = HTTP.request("POST", uri, headers, payload; require_ssl_verification = false, cookies = true, cookiejar = cookiejar)
-        Base.shred!(capass)
-        if response.status == 200
-                for set in cookiejar[pvwahost]
-                        if set.name == "CA66666"
-                                return set.value
-                                break
-                        end
-                end
-        else
-                error("Response not 200.")
-        end
-end
-
-function webreq(pvwauri, cookiejar, headerauth, query)
-        uri = "$pvwauri/api/$query"
-        headers = ["Content-Type" => "application/json", "X-CA66666" => headerauth ]
-        response = HTTP.request("GET", uri, headers; require_ssl_verification = false, cookies = true, cookiejar = cookiejar)
-        #return String(response.body)
-        if response.status == 200
-                return JSON.parse(String(response.body))
-        else
-                error(response.status)
-        end
-        # return response.body
-end
-
-# Make webreq a little more friendly
-function request(req)
-        try response = webreq(pvwauri, cookiejar, headerauth, req)
-                return response
-        catch e
-                if e.status == 401
-                        global headerauth = login(pvwahost, pvwauri, causer, capass)
-                        response = webreq(pvwauri, cookiejar, headerauth, req)
-                else
-                        return e.status
-                end
-        end
-end
-
-function getpsm(pvwauri, cookiejar, headerauth, accountid, reason, target)
-        uri = "$pvwauri/api/Accounts/$accountid/PSMConnect"
-        headers = ["Content-Type" => "application/json", "X-CA66666" => headerauth ]
-        payload = JSON.json(Dict(
-                "reason" => reason,
-                "ConnectionComponent" => "PSM-RDP",
-                "ConnectionParams" => Dict(
-                        "AllowMappingLocalDrives" => Dict(
-                                "value" => "Yes",
-                                "ShouldSave" => false
-                        ),
-                        "PSMRemoteMachine" => Dict(
-                                "value" => target,
-                                "ShouldSave" => false
-                        )
-                )
-        ))
-        response = HTTP.request("POST", uri, headers, payload; require_ssl_verification = false, cookies = true, cookiejar = cookiejar)
-        if response.status == 200
-                #return JSON.parse(String(response.body))
-                return String(response.body)
-        else
-                error(response.status)
-        end
-end
 
 function makedatastructures(caaccounts)
+        viewlist = Dict()
+        accountlist = dict()
         for acc in caaccounts["Accounts"]
                 # Alias data for sanity's sake
                 safe = (try acc["Properties"]["Safe"] catch; nothing end)
@@ -117,12 +31,12 @@ function makedatastructures(caaccounts)
                 # Add the root key for the safe if it doesn't exist yet.
                 try viewlist[safe] catch; global viewlist[safe] = Dict() end
                 # Add our account
-                global viewlist[safe][name] = Dict(
+                viewlist[safe][name] = Dict(
                         "Address" => addr,
                         "Username" => user,
                         "AccountID" => id
                 )
-                global accountlist[name] = Dict(
+                accountlist[name] = Dict(
                         "Address" => addr,
                         "Username" => user,
                         "AccountID" => id,
@@ -133,12 +47,40 @@ function makedatastructures(caaccounts)
 end
 
 
+######
+
+# Make webreq a little more friendly
+function request(cookieset, query)
+        try response = CyberArkPVWAClient.request(pvwauri, cookieset, query)
+                return response
+        catch e
+                if e.status == 401
+                        global capass = Base.getpass("Please enter your CyberArk password")
+                        global cookieset = CyberArkPVWAClient.login(pvwauri, method, causer, capass)
+                        response = CyberArkPVWAClient.request(pvwauri, cookieset, headerauth, query)
+                else
+                        return e.status
+                end
+        end
+end
+
+
 # Try a login initially.
-headerauth = login(pvwahost, pvwauri, causer, capass)
+#headerauth = login(pvwauri, method, causer, capass)
+capass = Base.getpass("Please enter your CyberArk password")
+cookieset = CyberArkPVWAClient.login(pvwauri, method, causer, capass)
 
 # List accounts. This variable stores the original format from the CyberArk API.
-caaccounts = request("ExtendedAccounts")
+caaccounts = CyberArkPVWAClient.request(pvwauri, cookieset, "ExtendedAccounts")
+
+# Using request will automatically prompt for password if the previous cookie expired.
+caaccounts = request(cookieset, "ExtendedAccounts")
+
+
 # Make our format data structures.
+# Used in makedatastructures
+viewlist = Dict()
+accountlist = Dict()
 makedatastructures(caaccounts)
 #accountlist and viewlist
 
@@ -150,7 +92,8 @@ reason = "Because I want to."
 
 accountid = accountlist[accname]["AccountID"]
 # Request the generated RDP file.
-psmstr = getpsm(pvwauri, cookiejar, headerauth, accountid, reason, target)
+
+psmstr = CyberArkPVWAClient.psmconnect(pvwauri, cookieset, accountid, reason, target)
 
 filename = "rdpswap.rdp"
 open(filename, "w")
